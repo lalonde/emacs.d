5906c0a9ec232a2ec50c9e20be40a7406796a400
Michael Lalonde <Michael_Lalonde@cable.comcast.com>
2016-09-13 16:55:00 -0400

    Fix go-eldoc startup issue

--- conf/lang-conf.el
+++ conf/lang-conf.el
@@ -6,9 +6,10 @@
   (c-set-offset 'arglist-intro '+))
 (add-hook 'java-mode-hook 'my-indent-setup)
 
-;;Go autocomplete
+;;Go
 (require 'go-autocomplete)
 (require 'auto-complete-config)
+(require 'go-eldoc)
 (ac-config-default)
 (defun my-go-mode-hook ()
   (add-hook 'before-save-hook #'gofmt-before-save)
@@ -17,5 +18,5 @@
   (go-eldoc-setup))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (set-face-attribute 'eldoc-highlight-function-argument nil
-                    :underline f :foreground "green"
+                    :underline nil :foreground "green"
                     :weight 'bold)
